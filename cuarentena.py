bedc981436146e9d524c5307f9515b1b5b424d2c
Ethel <89223773+alexaplaywoman@users.noreply.github.com>
2024-06-19 18:57:53 -0400

    Delete cuarentena.py

--- cuarentena.py
+++
@@ -1,7 +0,0 @@
-# Pone en cuarentena la ruta del directorio con posible scripts maliciosos
-import subprocess
- 
-def cuarentena(ruta):
-    ruta_cuarentena = '/cuarentena' 
-    comando = subprocess.run(['mv', ruta, ruta_cuarentena])
-
