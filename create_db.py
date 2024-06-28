aeac2aad3a23412cbd74552cf7ac77717395683c
Ethel <89223773+alexaplaywoman@users.noreply.github.com>
2024-06-27 22:11:57 -0400

    add create_db and create_table

--- create_db.py
+++ create_db.py
@@ -1,25 +1,56 @@
 import psycopg2
 
-conn = psycopg2.connect(database = "hips_data", 
-                        user = "postgres", 
-                        host= 'localhost',
-                        password = "postgresql",
-                        port = 5432)
+def create_db():
+    # Establish global connection to the default database
+    conn = psycopg2.connect(database="postgres", 
+                            user="postgres", 
+                            host="localhost",
+                            password="123457",
+                            port=5432)
 
+    # Enable autocommit (optional but recommended)
+    conn.autocommit = True
+    # Open a cursor within the global connection
+    cur = conn.cursor()
 
-# Open a cursor to perform database operations
-cur = conn.cursor()
-# Execute a command: create datacamp_courses table
-cur.execute("""CREATE TABLE accounts(
+    try:
+        # Execute DROP DATABASE outside a transaction
+        cur.execute("DROP DATABASE IF EXISTS hips_data;")
+
+        # Execute CREATE DATABASE outside a transaction
+        cur.execute("CREATE DATABASE hips_data;")
+        print("Database has been created successfully !!")
+    finally:
+        # Close the cursor (but keep the connection open)
+        cur.close()
+
+    # Close the connection to the default database
+    conn.close()
+
+def create_tables():
+    # Establish global connection to the default database
+    conn = psycopg2.connect(database="hips_data", 
+                            user="postgres", 
+                            host="localhost",
+                            password="123457",
+                            port=5432)
+
+    # Enable autocommit (optional but recommended)
+    conn.autocommit = True
+    # Open a cursor within the global connection
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE hash(
             id  SERIAL PRIMARY KEY,
-            username VARCHAR (50) UNIQUE NOT NULL,
-            password_hash VARCHAR (500) NOT NULL,
-            salt VARCHAR(100) NOT NULL),
-            hash_algo VARCHAR(10),
-            iterations INT;
-            """)
-# Make the changes to the database persistent
-conn.commit()
-# Close cursor and communication with the database
-cur.close()
-conn.close()
+            filename VARCHAR(255) NOT NULL,
+            hash_value VARCHAR(255) NOT NULL
+            );""")
+        # Make the changes to the database persistent
+    conn.commit()
+
+    print("Table has been created successfully !!")
+    # Close cursor and communication with the database
+    cur.close()
+    conn.close()
+
+create_db()
+create_tables()
